8dda2a2ab4fe3e4e738189ac312b445314eec45f
Numair <numairkaukab2@gmail.com>
2025-06-08 11:33:21 +0500

    performance tests

---
+++ pipelines/pipeline.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+    node {
+
+      stage('Initialise') {
+        /* Checkout the scripts */
+        checkout scm: [
+                $class: 'GitSCM',
+                userRemoteConfigs: [
+                        [
+                                url: "https://github.com/numairkaukab2/performance-test-repo.git",
+                                credentialsId: "numair-github"
+                        ]
+                ],
+                branches: [[name: "main"]]
+        ], poll: false
+      }
+
+      stage('Complete any setup steps') {
+        echo "Complete set-up steps"
+      }
+
+      stage('Execute Performance Tests') {
+        dir("${WORKSPACE}/scripts") {
+            bat 'c:/apache-jmeter/apache-jmeter/bin/jmeter.bat -n -t Shift-Left.jmx -l Shift-Left.jtl'
+        }
+      }
+
+      stage('Analyse Results') {
+        echo "Analyse results"
+      }
+    }
